c9c891dcb1363705137a3bd2aacf1fb2b22db15e
armengau <eric.armengaud@cea.fr>
2023-06-13 03:08:02 -0700

    include los interpolation in get_qso_deltas()

--- eBOSS_dr16_analysis.py
+++ eBOSS_dr16_analysis.py
@@ -7,7 +7,7 @@ from astropy.table import Table, vstack
 import scipy
 
 
-def get_qso_deltas(delta_file_name, qso_cat, lambda_min, lambda_max):
+def get_qso_deltas(delta_file_name, qso_cat, lambda_min, lambda_max, with_interpolation=False):
     """ This function returns a table of ra, dec, wavelength and delta for each of the QSOs in qso_cat.
     Wavelenghts are selected in [lambda_min, lambda_max]
 
@@ -25,6 +25,10 @@ def get_qso_deltas(delta_file_name, qso_cat, lambda_min, lambda_max):
     lambda_max: Float
     Value of the maximum forest wavelength required
     
+    with_interpolation: bool, default False
+    If True, interpolate the deltas on the reference BOSS wavelength grid.
+    In principle we dont have to use that.
+
     Return:
     -------
     los_table: Table
@@ -76,14 +80,25 @@ def get_qso_deltas(delta_file_name, qso_cat, lambda_min, lambda_max):
                 mask_wavelength = (wavelength > np.log10(lambda_min)) & (wavelength < np.log10(lambda_max))
                 #print('wavelength',len(wavelength[mask_wavelength]), wavelength[mask_wavelength])
                 
+                if with_interpolation:
+                    interp_fct = scipy.interpolate.UnivariateSpline(
+                        wavelength[mask_wavelength],
+                        delta_los[mask_wavelength],
+                        s=0)
+                    delta_los_interpolated = interp_fct(wavelength_ref)
+                    los_table[i]['ra'] = delta_i_header['RA'] * 180 / np.pi
+                    los_table[i]['dec'] = delta_i_header['DEC'] * 180 / np.pi
+                    los_table[i]['delta_los'] = delta_los_interpolated
+                    los_table[i]['wavelength'] = wavelength_ref
+
                 # Checking that the masked wavelength and wavelength_ref have the same shape
                 # otherwise it means that there are masked pixels
                 # and we don't want to consider this delta in the calculation
-                if len(wavelength[mask_wavelength]) == len(wavelength_ref):
+                elif len(wavelength[mask_wavelength]) == len(wavelength_ref):
                     
                     if np.allclose(wavelength[mask_wavelength], wavelength_ref):
                         los_table[i]['ra'] = delta_i_header['RA'] * 180 / np.pi  # must convert rad --> dec.
-                        los_table[i]['dec'] = delta_i_header['DEC'] * 180 / np.pi  # must convert rad --> dec.
+                        los_table[i]['dec'] = delta_i_header['DEC'] * 180 / np.pi
                         los_table[i]['delta_los'] = delta_los[mask_wavelength]
                         los_table[i]['wavelength'] = wavelength[mask_wavelength]
                     else:
@@ -98,66 +113,3 @@ def get_qso_deltas(delta_file_name, qso_cat, lambda_min, lambda_max):
     return los_table
 
 
-def get_qso_deltas_with_interp(delta_file_name, qso_cat):
-    """ This function returns a table of ra, dec, wavelength and delta for each of the QSOs in the qso_cat
-
-    Arguments:
-    ----------
-    delta_file_name: String
-    delta fits file
-    
-    qso_cat: String
-    qso_cat for which we want to find the corresponding deltas in delta_directory
-    
-    Return:
-    -------
-    all_los_table: Table
-    Table where each row corresponds to a QSO, containing [ra, dec, wavelength, delta_los]
-    """
-    
-    # Reading the THING_ID of each quasar in the catalog
-    qso_thing_id = np.array(qso_cat['THING_ID'])
-
-    # Initializing table all_los_table
-    all_los_table = Table()
-    
-    delta_file = fits.open(delta_file_name)
-    n_hdu = len(delta_file) # Each delta file contains many hdu
- 
-    for i in range(1, n_hdu):
-        delta_i_header = delta_file[i].header
-        delta_i_data = delta_file[i].data
-        delta_ID = delta_i_header['THING_ID']
-        
-        if delta_ID in qso_thing_id:
-            # Reading data
-            delta_los = np.array(delta_i_data['DELTA'])
-            wavelength = np.array(delta_i_data['LOGLAM'])
-            ra_coord = delta_i_header['RA'] * 180 / np.pi  # in rad and must be converted to degree
-            dec_coord = delta_i_header['DEC'] * 180 / np.pi
-            
-            # Interpolating deltas
-            wavelength_min = np.min(wavelength)
-            wavelength_max = np.max(wavelength)
-            n_wavelength = 300
-            wavelength_for_interpolation = np.linspace(wavelength_min, wavelength_max, n_wavelength)
-            interpolation_function = scipy.interpolate.UnivariateSpline(wavelength, delta_los, s=0)
-            delta_los_interpolated = interpolation_function(wavelength_for_interpolation)
-            
-            # Initializing table
-            delta_table = Table()
-            delta_table['ra'] = np.zeros(1)
-            delta_table['dec'] = np.zeros(1)
-            delta_table['delta_los'] = np.zeros((1, n_wavelength))
-            delta_table['wavelength'] = np.zeros((1, n_wavelength))
-            
-            # Filling table
-            delta_table['delta_los'] = delta_los_interpolated
-            delta_table['wavelength'] = wavelength_for_interpolation
-            delta_table['ra'] = ra_coord
-            delta_table['dec'] = dec_coord
-            
-            # Stacking
-            all_los_table = vstack([all_los_table, delta_table])
-            
-    return all_los_table
