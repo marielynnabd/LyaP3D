2daa08127db5d4a8ed85001f7a45f03a294c9c8d
armengau <eric.armengaud@cea.fr>
2023-06-29 12:22:22 +0200

    remove duplicates from pcross_interpolated

--- p3d_computation.py
+++ p3d_computation.py
@@ -78,6 +78,57 @@ def wavenumber_rebin(p3d_table, n_kbins):
     return p3d_table
 
 
+def _pcross_interpolated(pcross_table, angular_separation_array, n_angsep=1000,
+						interp_method='UnivariateSpline',
+						add_noise=False):
+	#- Compute Pcross_interpolated(kpar, angsep)
+	#      add_noise: if True, add gaussian fluctuations on top of Pcross
+	#				 (used to propagate Px error bars to P3D)
+	#      other args are described in main routines
+
+    # reading k_parallel from pcross_table
+    k_parallel = np.array(pcross_table['k_parallel'][0])
+
+	# Define a thinner binning of angular separations
+	ang_sep_min = np.min(angular_separation_array)
+	ang_sep_max = np.max(angular_separation_array)
+	if interp_method == 'none':
+		n_angsep = len(angular_separation_array)
+		angular_separation_array_fine_binning = angular_separation_array
+	else:
+		angular_separation_array_fine_binning = np.linspace(ang_sep_min, ang_sep_max, n_angsep)
+
+	Pcross_interpolated = np.zeros((len(k_parallel), n_angsep))
+	for ik_par, k_par in enumerate(k_parallel):  # k_par in [h/Mpc]
+		# Reading Pcross from table
+		try:
+			Pcross = np.array(pcross_table['corrected_power_spectrum'][:,ik_par])
+		except:
+			Pcross = np.array(pcross_table['mean_power_spectrum'][:,ik_par])
+
+		# Add fluctuations
+		if add_noise:
+			error_Pcross = np.array(pcross_table['error_power_spectrum'][:,ik_par])
+			Pcross = np.random.normal(Pcross, error_Pcross)
+
+		# Interpolating Pcross
+		if interp_method == 'none':
+			Pcross_interpolated[ik_par,:] = Pcross
+		else:
+			if interp_method == 'UnivariateSpline':
+				interpolation_function_Pcross = scipy.interpolate.UnivariateSpline(
+					angular_separation_array, Pcross, s=smoothing)
+			elif interp_method == 'PchipInterpolator':
+				interpolation_function_Pcross = scipy.interpolate.PchipInterpolator(
+					angular_separation_array, Pcross)
+			else:
+				raise ValueError('Wrong interp_method')
+			# Pcross_interpolated computation
+			Pcross_interpolated[ik_par,:] = interpolation_function_Pcross(angular_separation_array_fine_binning)
+
+	return (angular_separation_array_fine_binning, Pcross_interpolated)
+
+
 def pcross_to_p3d_cartesian(pcross_table, k_perpendicular, units_k_perpendicular,
                   mean_redshift, interp_method='UnivariateSpline', smoothing=0, n_angsep=1000,
                   compute_errors=False, k_binning=False):
@@ -146,12 +197,26 @@ def pcross_to_p3d_cartesian(pcross_table, k_perpendicular, units_k_perpendicular
     # mean_ang_separation is always saved in degrees it must be converted to Mpc/h if k_perp is defined in [Mpc/h]^-1
     if units_k_perpendicular == '[Mpc/h]^-1':
         angular_separation_array = np.array(pcross_table['mean_ang_separation']) * deg_to_Mpc * h # [Mpc/h]
-        
     else:
         angular_separation_array = np.array(pcross_table['mean_ang_separation']) # [degree]
-
-    # reading k_parallel from pcross_table
     k_parallel = np.array(pcross_table['k_parallel'][0])
+
+	ang_sep_finebin, Pcross_interpolated = _pcross_interpolated(
+							pcross_table, angular_separation_array,
+							n_angsep=n_angsep, interp_method=interp_method)
+
+	# Generating n_iterations of random Pcross and interpolating each before error_P3D computation
+	if compute_errors == True:
+		# Defining number of iterations of Pcross
+		n_iterations = 100
+		random_Pcross_interpolated = np.zeros((n_iterations, len(k_parallel), n_angsep))
+		for i in range(n_iterations):
+			_, pcross_fluct = _pcross_interpolated(pcross_table,
+							angular_separation_array,
+							n_angsep=n_angsep,
+							interp_method=interp_method,
+							add_noise=True)
+			random_Pcross_interpolated[i,:,:] = pcross_fluct
 
     # Initializing P3D table
     p3d_table = Table()
@@ -160,63 +225,14 @@ def pcross_to_p3d_cartesian(pcross_table, k_perpendicular, units_k_perpendicular
     p3d_table['P3D'] = np.zeros((len(k_perpendicular), len(k_parallel)))
     p3d_table['error_P3D'] = np.zeros((len(k_perpendicular), len(k_parallel)))
 
-	## Define a thinner binning of angular separations used for P3D and errorP3D computation
-	ang_sep_min = np.min(angular_separation_array)
-	ang_sep_max = np.max(angular_separation_array)
-	if interp_method == 'none':
-		angular_separation_array_fine_binning = angular_separation_array
-	else:
-		angular_separation_array_fine_binning = np.linspace(ang_sep_min, ang_sep_max, n_angsep)
-
-	## P3D computation
+	# Integrate Pcross_interpolated to compute P3D
 	for ik_par, k_par in enumerate(k_parallel):  # k_par in [h/Mpc]
-		# Reading Pcross from table
-		try:
-			Pcross = np.array(pcross_table['corrected_power_spectrum'][:,ik_par])
-		except:
-			Pcross = np.array(pcross_table['mean_power_spectrum'][:,ik_par])
-
-		# Interpolating Pcross
-		if interp_method == 'none':
-			Pcross_interpolated = Pcross
-		else:
-			if interp_method == 'UnivariateSpline':
-				interpolation_function_Pcross = scipy.interpolate.UnivariateSpline(
-					angular_separation_array, Pcross, s=smoothing)
-			elif interp_method == 'PchipInterpolator':
-				interpolation_function_Pcross = scipy.interpolate.PchipInterpolator(
-					angular_separation_array, Pcross)
-			else:
-				raise ValueError('Wrong interp_method')
-			# Pcross_interpolated computation
-			Pcross_interpolated = interpolation_function_Pcross(angular_separation_array_fine_binning)
-
-		# Generating n_iterations of random Pcross and interpolating each before error_P3D computation
-		if compute_errors == True:
-			# Reading error_Pcross from table
-			error_Pcross = np.array(pcross_table['error_power_spectrum'][:,ik_par])
-
-			# Defining number of iterations of Pcross
-			n_iterations = 100
-			random_Pcross = np.random.normal(Pcross, error_Pcross, (n_iterations, len(Pcross)))
-			random_Pcross_interpolated = np.zeros((n_iterations, n_angsep))
-			for i in range(n_iterations):
-				if interp_method == 'spline interpolation':
-					interpolation_function_random_Pcross = scipy.interpolate.UnivariateSpline(
-						angular_separation_array, random_Pcross[i,:], s=smoothing)
-				else: # this means that method == 'pchip interpolation' since 'no interpolation has been already checked'
-					interpolation_function_random_Pcross = scipy.interpolate.PchipInterpolator(
-						angular_separation_array, random_Pcross[i,:])
-
-				random_Pcross_interpolated[i,:] = interpolation_function_random_Pcross(
-					angular_separation_array_fine_binning)
-
 		for ik_perp, k_perp in enumerate(k_perpendicular): # k_perp in [h/Mpc]
 			#  Defining integrand_Pcross
-			integrand_Pcross = 2 * np.pi * angular_separation_array_fine_binning * scipy.special.j0(angular_separation_array_fine_binning * k_perp) * Pcross_interpolated
+			integrand_Pcross = 2 * np.pi * ang_sep_finebin * scipy.special.j0(ang_sep_finebin * k_perp) * Pcross_interpolated[ik_par,:]
 
 			# Computing integral to get P3D
-			P3D = np.trapz(integrand_Pcross, angular_separation_array_fine_binning)
+			P3D = np.trapz(integrand_Pcross, ang_sep_finebin)
 
 			# Filling table
 			p3d_table['k_parallel'][ik_perp,ik_par] = k_par
@@ -224,7 +240,7 @@ def pcross_to_p3d_cartesian(pcross_table, k_perpendicular, units_k_perpendicular
 
 			if compute_errors == True:
 				# Defining integrand_random_Pcross
-				integrand_random_Pcross = 2 * np.pi * angular_separation_array_fine_binning * scipy.special.j0(angular_separation_array_fine_binning * k_perp) * random_Pcross_interpolated
+				integrand_random_Pcross = 2 * np.pi * ang_sep_finebin * scipy.special.j0(ang_sep_finebin * k_perp) * random_Pcross_interpolated[:,ik_par,:]
 
 				# Computing integral to get P3D
 				random_P3D = np.trapz(integrand_random_Pcross, angular_separation_array_fine_binning, axis=-1)
@@ -243,7 +259,7 @@ def pcross_to_p3d_cartesian(pcross_table, k_perpendicular, units_k_perpendicular
 
 
 def pcross_to_p3d_polar(pcross_table, mu_array, mean_redshift, input_units='Mpc/h', output_units='Mpc/h',
-                        interp_method='spline interpolation', smoothing=0, n_angsep=1000, compute_errors=False):
+                        interp_method='UnivariateSpline', smoothing=0, n_angsep=1000, compute_errors=False):
     """ This function computes the P3D out of the Pcross in polar coordinates:
           - It either computes the P3D out of Pcross by direct integration over the angular separation
           - Or it interpolates the Pcross with a spline function before integration
@@ -325,20 +341,16 @@ def pcross_to_p3d_polar(pcross_table, mu_array, mean_redshift, input_units='Mpc/
     # reading k_parallel from pcross_table
     k_parallel = np.array(pcross_table['k_parallel'][0])
 
+	ang_sep_finebin, Pcross_interpolated = _pcross_interpolated(
+							pcross_table, angular_separation_array,
+							n_angsep=n_angsep, interp_method=interp_method)
+
     # Initializing P3D table
     p3d_table = Table()
     p3d_table['mu'] = np.array(mu_array)
     p3d_table['k'] = np.zeros((len(mu_array), len(k_parallel))) # len of k and k_parallel is the same
     p3d_table['P3D'] = np.zeros((len(mu_array), len(k_parallel)))
     p3d_table['error_P3D'] = np.zeros((len(mu_array), len(k_parallel)))
-
-	## Define a thinner binning of angular separations used for P3D and errorP3D computation
-	ang_sep_min = np.min(angular_separation_array)
-	ang_sep_max = np.max(angular_separation_array)
-	if interp_method == 'none':
-		angular_separation_array_fine_binning = angular_separation_array
-	else:
-		angular_separation_array_fine_binning = np.linspace(ang_sep_min, ang_sep_max, n_angsep)
 
 	## P3D computation
 	for i_mu, mu in enumerate(mu_array):
@@ -349,28 +361,11 @@ def pcross_to_p3d_polar(pcross_table, mu_array, mean_redshift, input_units='Mpc/
 			k = k_par / mu
 			k_perp = np.sqrt(k**2 - k_par**2)
 
-			# Reading corresponding Pcross from table
-			Pcross = np.array(pcross_table['mean_power_spectrum'][:,ik_par])
-
-			# Interpolating Pcross
-			if interp_method == 'none':
-				Pcross_interpolated = Pcross
-			else:
-				if interp_method == 'UnivariateSpline':
-					interpolation_function_Pcross = scipy.interpolate.UnivariateSpline(
-						angular_separation_array, Pcross, s=smoothing)
-				elif interp_method == 'PchipInterpolator':
-					interpolation_function_Pcross = scipy.interpolate.PchipInterpolator(
-						angular_separation_array, Pcross)
-				else:
-					raise ValueError('Wrong interp_method')
-				Pcross_interpolated = interpolation_function_Pcross(angular_separation_array_fine_binning)
-
 			# Defining integrand_Pcross
-			integrand_Pcross = 2 * np.pi * angular_separation_array_fine_binning * scipy.special.j0(angular_separation_array_fine_binning * k_perp) * Pcross_interpolated
+			integrand_Pcross = 2 * np.pi * ang_sep_finebin * scipy.special.j0(ang_sep_finebin * k_perp) * Pcross_interpolated[ik_par,:]
 
 			# Computing integral to get P3D
-			P3D = np.trapz(integrand_Pcross, angular_separation_array_fine_binning)
+			P3D = np.trapz(integrand_Pcross, ang_sep_finebin)
 
 			# Filling table
 			p3d_table['k'][i_mu,ik_par] = k
