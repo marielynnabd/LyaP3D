f9459acfd2b9fd15cafe5d5c584abd6e459521cb
marielynnabd <marie-lynn.abdulkarim@cea.fr>
2024-03-01 09:21:23 -0800

    adding a masking to wavelength_ref similarly to data

--- LOS_info_DESI_Y1_analysis.py
+++ LOS_info_DESI_Y1_analysis.py
@@ -13,7 +13,7 @@ from .tools import SPEED_LIGHT
 from .DESI_Y1_analysis import _spectral_resolution
 
 
-def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_max, z_center, include_snr_reso=False):
+def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_max, z_center, lambda_pixelmask_min=None, lambda_pixelmask_max=None, include_snr_reso=False):
     """ This function returns a table of ra, dec, TARGETID, MEANRESO and MEANSNR for each of the QSOs in qso_cat
     Wavelenghts are selected in [lambda_min, lambda_max]
 
@@ -34,6 +34,12 @@ def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_ma
     z_center: Float or array of floats
     Value of z center per redshift bin
     PS: lambda_min, lambda_max and z_center must have the same length
+    
+    lambda_pixelmask_min: Float or array of floats
+    Minimum of wavelength intervals to be masked. This will be applied not on data, but on wavelength_ref
+    
+    lambda_pixelmask_max: Float or array of floats
+    Maximum of wavelength intervals to be masked. This will be applied not on data, but on wavelength_ref
 
     include_snr_reso: bool, default False
     If set, includes MEANRESO and MEANSNR from the delta's headers, to the los_info_table
@@ -47,11 +53,27 @@ def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_ma
     # Checking if z_center is array or float
     if hasattr(z_center,'__len__') is False:
         z_center = np.array([z_center])
+        
+    # Checking if lambda_pixelmask_min and lambda_pixelmask_max are arrays or floats, only if they're not none
+    if hasattr(lambda_pixelmask_min,'__len__') is False:
+        lambda_pixelmask_min = np.array([lambda_pixelmask_min])
+    if hasattr(lambda_pixelmask_max,'__len__') is False:
+        lambda_pixelmask_max = np.array([lambda_pixelmask_max])
     
     # Reference DESI wavelength grid
     wavelength_ref_min = 3600.  # AA
     wavelength_ref_max = 9824.  # AA
     delta_lambda = 0.8  # AA
+    wavelength_ref = np.arange(wavelength_ref_min, wavelength_ref_max+0.01, delta_lambda) # same all the time but need to be redefined
+    
+    # Masking pixels if masks is not none
+    if (lambda_pixelmask_min is not None) & (lambda_pixelmask_max is not None):
+        if (len(lambda_pixelmask_min)==len(lambda_pixelmask_max)):
+            for i in range(len(lambda_pixelmask_min)): # or lambda_pixelmask_max, it's the same
+                pixels_to_mask = (wavelength_ref > lambda_pixelmask_min[i]) & (wavelength_ref < lambda_pixelmask_max[i])
+                wavelength_ref = wavelength_ref[~pixels_to_mask]
+        else:
+            print('lambda_pixelmask_min and lambda_pixelmask_max have different lengths, therefore the mask is not taken into acccount')
 
     # Reading the TARGETID of each quasar in the catalog
     qso_tid = np.array(qso_cat['TARGETID'])
@@ -88,9 +110,10 @@ def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_ma
             wavelength = delta_file[i+1]['LAMBDA'][:].astype(float) 
                 
             for j in range(len(z_center)): # los_info_table_list must have n_zbins lists
-                wavelength_ref = np.arange(wavelength_ref_min, wavelength_ref_max+0.01, delta_lambda) # same all the time but need to be redefined
-                mask_wavelength_ref = (wavelength_ref > lambda_min[j]) & (wavelength_ref < lambda_max[j])
-                wavelength_ref = wavelength_ref[mask_wavelength_ref]
+                # wavelength_ref = np.arange(wavelength_ref_min, wavelength_ref_max+0.01, delta_lambda) # same all the time but need to be redefined
+                wavelength_ref_zbin = wavelength_ref # Where wavelength_ref is one for all zbins
+                mask_wavelength_ref_zbin = (wavelength_ref_zbin > lambda_min[j]) & (wavelength_ref_zbin < lambda_max[j])
+                wavelength_ref_zbin = wavelength_ref_zbin[mask_wavelength_ref_zbin]
                 
                 # This part is to check if the delta must be included in the redshift bin or not: 
                 # Checking if LAMBDA.min < lambda_min & LAMBDA.max > lambda_max
@@ -98,10 +121,10 @@ def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_ma
                     # Define wavelength mask
                     mask_wavelength = (wavelength > lambda_min[j]) & (wavelength < lambda_max[j])
 
-                    # Checking that the masked wavelength and wavelength_ref have the same shape 
+                    # Checking that the masked wavelength and wavelength_ref_zbin have the same shape 
                     # otherwise it means that there are masked pixels and we don't want to consider this delta in the calculation
-                    if len(wavelength[mask_wavelength]) == len(wavelength_ref):
-                        if np.allclose(wavelength[mask_wavelength], wavelength_ref):
+                    if len(wavelength[mask_wavelength]) == len(wavelength_ref_zbin):
+                        if np.allclose(wavelength[mask_wavelength], wavelength_ref_zbin):
                             los_info_table_list[j][i]['ra'] = delta_i_header['RA'] * 180 / np.pi  # must convert rad --> dec.
                             los_info_table_list[j][i]['dec'] = delta_i_header['DEC'] * 180 / np.pi
                             los_info_table_list[j][i]['TARGETID'] = delta_ID
@@ -131,7 +154,7 @@ def get_desi_los_info_singlefile(delta_file_name, qso_cat, lambda_min, lambda_ma
     return los_info_table_list
 
 
-def get_los_info_table_desi(qso_cat, deltas_dir, lambda_min, lambda_max, z_center, outputdir, outputfilename, ncpu='all', include_snr_reso=False):
+def get_los_info_table_desi(qso_cat, deltas_dir, lambda_min, lambda_max, z_center, outputdir, outputfilename, lambda_pixelmask_min=None, lambda_pixelmask_max=None, ncpu='all', include_snr_reso=False):
     """ This function returns a table of ra, dec, TARGETID, MEANRESO, MEANSNR, for each of the QSOs in qso_cat.
     Wavelenghts are selected in [lambda_min, lambda_max]
     Wrapper around get_los_info_singlefile
@@ -183,7 +206,7 @@ def get_los_info_table_desi(qso_cat, deltas_dir, lambda_min, lambda_max, z_cente
     with Pool(ncpu) as pool:
         output_get_desi_los_info_singlefile = pool.starmap(
             get_desi_los_info_singlefile,
-            [[f, qso_cat, lambda_min, lambda_max, z_center, include_snr_reso] for f in deltafiles]
+            [[f, qso_cat, lambda_min, lambda_max, z_center, lambda_pixelmask_min, lambda_pixelmask_max, include_snr_reso] for f in deltafiles]
         )
 
     for x in output_get_desi_los_info_singlefile:
