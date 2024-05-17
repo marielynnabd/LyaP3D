fa1c94d52ff0894f180d3826d70e0a0997b508eb
marielynnabd <marie-lynn.abdulkarim@cea.fr>
2024-05-17 08:07:38 -0700

    Adding add_missing_args_to_Nyxmock and list_of_allowed_qso

--- Nyx_QQ_mock_creation.py
+++ Nyx_QQ_mock_creation.py
@@ -11,10 +11,90 @@ sys.path.insert(0, os.environ['HOME']+'/Software/LyaP3D')
 from tools import LAMBDA_LYA
 
 
-def add_missing_args_to_Nyxmock(Nyx_mock_file, )
+def add_missing_args_to_Nyxmock(Nyx_mock_file, zbin_min, zbin_max):
+    """ This function adds the missing arguments 'z_qso', 'qso_id', 'hpix' to Nyxmock, so that it contains the arguments required for QQ
+    It is the adapted using the following function
+    
+    Arguments:
+    ----------
+    Nyx_mock_file: String
+    The mock file containing a fits table with only 1 HDU, where each row corresponds to a QSO, obtained using draw_los in mock_generation (transmissions)
+
+    zbin_min: Float
+    Minimum redshift of bin
+    
+    zbin_max: Float
+    Maximum redshift of bin
+
+    Return:
+    -------
+    Nyx_mock: Fits table
+    It will contain [qso_id, z_qso, ra, dec, hpix, wavelength, transmission_los], same as Nyx_mock in input but with added args
+    """
+    
+    Nyx_mock = Table.read(Nyx_mock_file)
+    
+    # Computing allowed z_qso
+    allowed_z_qso, tid_qso = list_of_allowed_qso(zbin_min, zbin_max)
+    
+    # Adding z_qso and qso_id
+    # Nyx_mock['z_qso'] = (np.max(Nyx_mock['wavelength']) + 100) / LAMBDA_LYA - 1
+    # Nyx_mock['qso_id'] = np.random.randint(1000, 3000, len(Nyx_mock))
+    random_index = np.random.randint(0, len(z_qso), len(Nyx_mock))
+    Nyx_mock['z_qso'] = allowed_z_qso[random_index]
+    Nyx_mock['tid_qso'] = tid_qso[random_index]
+
+    # Adding hpix
+    nside = 16
+    nest = True
+    Nyx_mock['hpix'] = hp.ang2pix(nside, Nyx_mock['ra'], Nyx_mock['dec'], nest, lonlat=True)
+    
+    return Nyx_mock
 
 
-def adapt_Nyxmock_to_QQ_input(Nyx_mock_file, outdir, healpix_nside, healpix_nest):
+def list_of_allowed_qso(zbin_min, zbin_max):
+    """ This function returns the list of allowed z_qso from QSO cat of DESI IRON in a way that their forest covers the forest range chosen by zbin_min and zbin_max.
+    This list will be different as we change the bins
+
+    Arguments:
+    ----------
+    zbin_min: Float
+    Minimum redshift of bin
+    
+    zbin_max: Float
+    Maximum redshift of bin
+
+    Return:
+    -------
+    z_qso_list: Array of floats
+    List of allowed z_qso
+    
+    qso_tid_list: Array of floats
+    List of correspodning TARGETIDs. PS: This is just to assign a TARGETID to our QSOs, but doesn't effectively change anything in the computations
+    """
+    
+    # Loading DESI IRON QSO catalog
+    qso_cat = Table.read('/global/homes/m/mabdulka/P3D/DESI_IRON_analysis/catalog_iron_v0_qso_target_nobal_BI.fits.gz')
+    
+    # Min and max wavelengths of chosen bin
+    lambdabin_min = (1 + zbin_min) * LAMBDA_LYA
+    lambdabin_max = (1 + zbin_max) * LAMBDA_LYA
+    
+    # Selecting allowed QSOs in this redshift bin
+    lambda_lyaforest_min = 1060 # Value in rest frame
+    lambda_lyaforest_max = 1200 # Value in rest frame
+    qso_lambda_lyaforest_min = (1 + qso_cat['Z']) * lambda_lyaforest_min # Observed wavelengths
+    qso_lambda_lyaforest_max = (1 + qso_cat['Z']) * lambda_lyaforest_max # Observed wavelengths
+    select_qso = (qso_lambda_lyaforest_min < lambdabin_min) & (qso_lambda_lyaforest_max > lambdabin_max)
+    
+    # Allowed z_qso and corresponding tid
+    z_qso_list = np.array(qso_cat['Z'][select_qso])
+    qso_tid_list = np.array(qso_cat['TARGETID'][select_qso])
+    
+    return z_qso_list, qso_tid_list
+
+
+def adapt_Nyxmock_to_QQ_input(Nyx_mock, outdir, healpix_nside, healpix_nest):
     """ This function first reads a mock of LOS transmissions created from a Nyx simulation 
     using the draw_los function in mock_generation.py and adapts it to the input format accepted by Quickquasars (i.e. fits table to fits image)
     It treats separately the LOS by healpix_pixel subsets and creates separate output transmission files for each healpix_pixel (as required by QQ)
@@ -29,8 +109,8 @@ def adapt_Nyxmock_to_QQ_input(Nyx_mock_file, outdir, healpix_nside, healpix_nest
 
     Arguments:
     ----------
-    Nyx_mock_file: String
-    The mock file containing a fits table with only 1 HDU, where each row corresponds to a QSO.
+    Nyx_mock: Fits table
+    The fits table contains only 1 HDU, where each row corresponds to a QSO.
     It must contain [qso_id, z_qso, ra, dec, hpix, wavelength, transmission_los]
     
     outdir: String
@@ -49,8 +129,8 @@ def adapt_Nyxmock_to_QQ_input(Nyx_mock_file, outdir, healpix_nside, healpix_nest
     The function might several outputs depending on the different hpix in the input mock, where each of the outputs is written in outdir
     """
 
-    # Reading input mock
-    Nyx_mock = Table.read(Nyx_mock_file)
+    # # Reading input mock
+    # Nyx_mock = Table.read(Nyx_mock_file)
     
     # Checking for different hpix
     all_pixels = set(Nyx_mock['hpix'])
